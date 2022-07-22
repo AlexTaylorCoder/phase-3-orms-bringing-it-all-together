c3dc5e96576d6287005222549c8b7f3ba887df82
alextaylor <alextaylor515@gmail.com>
2022-07-22 15:06:15 -0400

    commit tree 1bb10b411093e5b8a01fc9f0597b37009b2b8e2f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,75 @@
 class Dog
+    attr_accessor :id,:name,:breed
 
+    @@all = []
+
+    def initialize (id:nil, name:"Fido", breed:"lab")
+        @id = id
+        @name = name
+        @breed = breed
+    end
+    def self.create_table
+        sql = <<-SQL 
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+            SQL
+            DB[:conn].execute(sql)
+    end
+    def self.drop_table
+        sql = <<-SQL
+            DROP TABLE IF EXISTS dogs
+            SQL
+            DB[:conn].execute(sql)
+    end
+    def save 
+        if self.id
+            self.update
+        else 
+            sql = <<-SQL
+            INSERT INTO dogs (name,breed)
+            VALUES (?,?)
+            SQL
+            DB[:conn].execute(sql, self.name, self.breed)
+            self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        end
+        self
+    end
+    def self.update 
+        sql = <<-SQL
+                UPDATE dogs SET name=?,breed=? WHERE id=?
+                SQL
+                DB[:conn].execute(sql,self.name,self.breed,self.id)
+        end
+    def self.create name:, breed:
+        ndog = self.new(name: name, breed: breed)
+        ndog.save
+    end
+    def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+    end
+    def self.all
+        alltable = DB[:conn].execute("SELECT * FROM dogs")
+        alltable.map do |row|
+            self.new_from_db(row)
+        end
+    end
+    def self.find_by_name name
+        sql = <<-SQL
+        SELECT * FROM dogs WHERE name= ?
+        SQL
+        DB[:conn].execute(sql,name).map do |row|
+            self.new_from_db(row)
+        end[0]
+    end
+    def self.find id
+        sql= <<-SQL
+        SELECT * FROM dogs WHERE id=?
+        SQL
+        DB[:conn].execute(sql,id).map do |row|
+            self.new_from_db(row)
+        end[0]
+    end
 end
